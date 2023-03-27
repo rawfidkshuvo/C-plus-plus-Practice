f713f02aa81704d0c091eed4d1b999ac2ea1ae02
rawfidkshuvo <129013074+rawfidkshuvo@users.noreply.github.com>
2023-03-27 03:18:42 +0200

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a[] = {1,2,3,4,6,8,10};
-    int temp = 1;
-    for (int i=0; i<sizeof(a)/sizeof(int); i++){
-        if(a[i] == temp){
-            temp++;
-        }
-        else{
-            cout << temp;
-            i--;
-            temp++;
-        }
-    }
-    
+    cout << "Hello World!"
     return 0;
 }
