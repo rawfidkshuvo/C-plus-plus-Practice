9b815f03d82bc114ef487b0e9932245e0890faf2
rawfidkshuvo <rawfid.shuvo@outlook.com>
2023-03-31 23:32:41 +0200

    Update decimal2binary.cpp
    Added binary to decimal.

--- C++ Projects/decimal2binary.cpp
+++ C++ Projects/decimal2binary.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 void decimalToBinary(int decimal){
     int i = 0;
     int bin[100];
+
     while(decimal != 0){
     int remainder = decimal % 2;
     decimal = decimal / 2;
@@ -14,9 +16,30 @@ void decimalToBinary(int decimal){
         cout << bin[j];
     }
 }
+
+void binaryToDecimal(int binary){
+
+       int  decimal = 0, remainder, i = 0;
+        while(binary != 0){
+        remainder = binary % 10;
+        binary /= 10;
+        decimal += remainder * pow(2,i);
+        i++;
+        }
+
+        cout << decimal;
+}
+
+
 int main(){
     int decimal;
+    int binary;
     cout << "Enter a decimal number: ";
     cin >> decimal;
     decimalToBinary(decimal);
+    cout << endl;
+    
+    cout << "Enter a binary number: ";
+    cin >> binary;
+    binaryToDecimal(binary);
 }
